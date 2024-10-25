a58c4137bf63a595f0ef30e3381db9290ee51b50
Alamgir Khan <alamgirkhqwer@gmail.com>
2024-10-25 23:49:40 +0500

    APIs done (Fetch, del, Add)

--- src/assets/Components/AddProduct.jsx
+++ src/assets/Components/AddProduct.jsx
@@ -1,26 +1,37 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
-const AddProduct = () => {
+const AddProduct = (prop) => {
 
-    const [Title, setTitle] = useState()
-    const [Price, setPrice] = useState()
-    const [DisPrice, setDisPrice] = useState()
-    const [Discription, setDiscription] = useState()
-    const AddProduct = () => { alert("Add Product") }
+    const [Product, setProduct] = useState({ Title: "", Discounted_Price: "", Price: "", Discription: "" })
+
+    const AddProduct = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:5000/Product', Product)
+            .then(res => {
+                console.log(res)
+                alert("New data inserted")
+                prop.FetchData()
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
-        <div className='flex justify-center gap-10 mt-8'>
-            <input onChange={(e) => { setTitle(e.target.value) }} type="text" placeholder='Title' className='p-2 border border-gray-200 rounded-xl' />
-            <input onChange={(e) => { setPrice(e.target.value) }} type="text" placeholder='Price' className='p-2 border border-gray-200 rounded-xl' />
-            <input onChange={(e) => { setDisPrice(e.target.value) }} type="text" placeholder='Discount price' className='p-2 border border-gray-200 rounded-xl' />
-            <input onChange={(e) => { setDiscription(e.target.value) }} type="text" placeholder='Discription' className='p-2 border border-gray-200 rounded-xl' />
 
-            <div className="AddButton w-fit p-2 px-4 bg-green-500 rounded-xl text-white font-bold cursor-pointer
+        <div className='mt-8'>
+
+            <form className='flex justify-center gap-10' onSubmit={AddProduct}>
+                <input onChange={(e) => { setProduct({ ...Product, Title: e.target.value }) }} type="text" placeholder='Title' className='p-2 border border-gray-200 rounded-xl' />
+                <input onChange={(e) => { setProduct({ ...Product, Discounted_Price: e.target.value }) }} type="text" placeholder='Price' className='p-2 border border-gray-200 rounded-xl' />
+                <input onChange={(e) => { setProduct({ ...Product, Price: e.target.value }) }} type="text" placeholder='Discount price' className='p-2 border border-gray-200 rounded-xl' />
+                <input onChange={(e) => { setProduct({ ...Product, Discription: e.target.value }) }} type="text" placeholder='Discription' className='p-2 border border-gray-200 rounded-xl' />
+
+                <button className="AddButton w-fit p-2 px-4 bg-green-500 rounded-xl text-white font-bold cursor-pointer
             hover:bg-green-600 hover:transition-all hover:scale-105 hover:shadow-lg"
-                onClick={AddProduct}>
-                Add
-            </div>
-
+                    type='submit'>
+                    Add
+                </button>
+            </form>
         </div>
     )
 }
--- src/assets/Components/Product.jsx
+++ src/assets/Components/Product.jsx
@@ -5,17 +5,32 @@ import axios from 'axios'
 const Projuct = () => {
 
     const [ToggleAddProjuct, setToggleAddProjuct] = useState(false)
-    // const [Title, setTitle] = useState()
-    // const [Products, setProducts] = useState[""]
+    const [Title, setTitle] = useState([])
+    const [Products, setProducts] = useState([])
+
+    const FetchData = () => {
+        axios.get('http://localhost:5000/Product')
+            .then(res => {
+                console.log(Object.keys(res.data[0]))
+                setTitle(Object.keys(res.data[0]))
+                setProducts(res.data)
+            })
+            .catch(err => console.log(err))
+    }
 
     useEffect(() => {
-        const FetchData = () => {
-            axios.get('http://localhost:5000/Product')
-                .then(res => console.log(res.data))
-                .catch(err => console.log(err))
-        }
         FetchData()
     }, []);
+
+    function DeleteProduct(id) {
+        axios.delete(`http://localhost:5000/Product/` + id)
+            .then(res => {
+                console.log(res)
+                alert("Product deleted")
+                FetchData()
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className='container mx-auto p-10 my-10 bg-white rounded-2xl'>
@@ -26,15 +41,39 @@ const Projuct = () => {
             </div>
 
             <div className="Add-Product">
-                {ToggleAddProjuct === true && <div className='transition-all'><AddProduct /></div>}
+                {ToggleAddProjuct === true && <div className='transition-all'><AddProduct FetchData={FetchData} /></div>}
                 <hr className='mt-5' />
             </div>
 
             <div className="productView">
-                fetch data here
+                <table className='w-full'>
+                    <thead>
+                        <tr>
+                            {Title.map((d, i) => (
+                                <th className='pt-5'>{d}</th>
+                            ))}
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {Products.map((d, i) => (
+                            <tr key={i}>
+                                <td className='border-r border-gray-200 pl-5 text-center'>{d.id}</td>
+                                <td className='border-r border-gray-200 pl-5'>{d.Title}</td>
+                                <td className='border-r border-gray-200 pl-5'>{d.Price}</td>
+                                <td className='border-r border-gray-200 pl-5'>{d.Discounted_Price}</td>
+                                <td className='border-r border-gray-200 pl-5'>{d.Discription}</td>
+
+                                <th className='p-2 text-red-500 rounded-xl cursor-pointer hover:transition transform hover:scale-105'
+                                    onClick={() => DeleteProduct(d.id)}>Delete</th>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
 
-        </div>
+        </div >
     )
 }
 
